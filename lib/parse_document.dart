c20741fc31f5a6e2e44208f4fa3800e4685e9f7f
paddykool <pjmullan@gmail.com>
2021-12-08 17:04:26 +0700

    updating parser to using string similarity - still in dev

--- lib/parse_document.dart
+++ lib/parse_document.dart
@@ -1,34 +1,76 @@
 import 'package:html/dom.dart'; // Contains DOM related classes for extracting data from elements
 import 'package:collection/collection.dart';
+import 'package:string_similarity/string_similarity.dart';
 
 Map<String, dynamic> getItemDetails({required Document document}) {
   Map<String, dynamic> data = {'title': '???', 'imageURL': '???', 'price': '???'};
 
   // Get the item name
   List<Element> h1s = document.getElementsByTagName('h1');
-  print('Number of titles found: ${h1s.length}');
+  print('Number of h1s found: ${h1s.length}');
+
   if (h1s.length != 0) {
-    print("title found: ${h1s[0].text.trim()}");
-    data["title"] = h1s[0].text.trim();
+    print('h1 raw: ${h1s[0].text}');
+    print('h1 with trim: ${h1s[0].text.trim()}');
+    print("first h1 found: ${h1s[0].text.trim()}");
+    String removeMultiSpace = h1s[0].text.replaceAll(RegExp(r"\s+"), " ");
+    print('removeMultiSpace: $removeMultiSpace');
+    data["title"] = removeMultiSpace.trim();
   }
 
   // Find the image URL
   var images = document.getElementsByTagName('img');
   print('Number of images found: ${images.length}');
 
-  // Find the image with the same alt as the title
-  // TODO add error condition here
-  Element? productImage = images.firstWhereOrNull((image) {
-    Map<Object, String> attributeMap = image.attributes;
+  // --------------------------------------------------------------------------------
+  //  Find the image element using ... shite
+  //
+  String itemTitle = data["title"];
+  // Element altSimilarImage = images.reduce((curr, next) => curr.attributes['alt'].similarityTo(itemTitle) > next.attributes['alt'].similarityTo(itemTitle) ? curr : next);
 
-    return attributeMap.containsKey('alt') &&
-        attributeMap['alt'] != null &&
-        attributeMap['alt']!.length > 10 &&
-        attributeMap['alt']!.substring(0, 9) == data["title"].substring(0, 9);
-    // TODO the above line should be changed to 'contains()'
+  Element altSimilarImage = images.reduce((curr, next) {
+    print('curr element alt text: ${curr.attributes['alt']}');
+    print('curr element alt similar value: ${curr.attributes['alt'].similarityTo(itemTitle)}');
+    print('next element alt text: ${next.attributes['alt']}');
+    print('next element alt similar value: ${next.attributes['alt'].similarityTo(itemTitle)}');
+    print('-----------------------');
+
+    if (curr.attributes['alt'].similarityTo(itemTitle) >=
+        next.attributes['alt'].similarityTo(itemTitle)) {
+      return curr;
+    } else {
+      return next;
+    }
   });
 
+  print('alt property of Element from string similiar: ${altSimilarImage.attributes['alt']}');
+
+  for (var image in images) {
+    Map<Object, String> attributeMap = image.attributes;
+    if (attributeMap.containsKey('alt')) {
+      print('Image contains alt and value is: ${attributeMap['alt']}');
+    }
+  }
+
+  //
+
+  // --------------------------------------------------------------------------------
+
+  // Find the image with the same alt as the title
+  // TODO add error condition here
+  // Element? productImage = images.firstWhereOrNull((image) {
+  //   Map<Object, String> attributeMap = image.attributes;
+  //
+  //   return attributeMap.containsKey('alt') &&
+  //       attributeMap['alt'] != null &&
+  //       attributeMap['alt']!.length > 10 &&
+  //       ((attributeMap['alt']?.replaceAll(RegExp(r"\s+"), " ").contains(data['title']) ?? false) || (attributeMap['title']?.contains(data['alt'].replaceAll(RegExp(r"\s+"), " ")) ?? false))
+  //       //attributeMap['alt']!.substring(0, 9) == data["title"].substring(0, 9);
+  //   // TODO the above line should be changed to 'contains()'
+  // });
+
   // Now get the source value
+  Element productImage = altSimilarImage;
   if (productImage != null) {
     print('now checking the source attribute of the productImage...');
 
